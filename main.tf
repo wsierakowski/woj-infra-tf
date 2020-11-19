759b76846535029c5835e61ee8d02d9424ea7d96
wsierakowski <wsierakowski@gmail.com>
2020-11-19 14:53:44 +0000

    enable versioning

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+    region = "eu-west-1"
+}
+
+resource "aws_s3_bucket" "my_s3_bucket" {
+    bucket = "my-example-20201119"
+    acl = "private"
+
+    versioning {
+      enabled = true
+    }
+
+    tags = {
+      Terraform: "true"
+    }
+}
