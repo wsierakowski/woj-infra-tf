9ba94ce95e9995f877f3e5060c09cb15c95ffc2a
wsierakowski <wsierakowski@gmail.com>
2020-11-19 14:18:15 +0000

    add terraform tag

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+    region = "eu-west-1"
+}
+
+resource "aws_s3_bucket" "my_s3_bucket" {
+    bucket = "my-example-20201119"
+    acl = "private"
+
+    tags = {
+      Terraform: "true"
+    }
+}
