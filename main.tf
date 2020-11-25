f79e2a6da67fadb8c313133ec2f12581061ce698
wsierakowski <wsierakowski@gmail.com>
2020-11-25 10:03:50 +0000

    adding cost center tag

--- main.tf
+++ main.tf
@@ -1,16 +1,17 @@
 provider "aws" {
-    region = "eu-west-1"
+  region = "eu-west-1"
 }
 
 resource "aws_s3_bucket" "my_s3_bucket" {
-    bucket = "my-example-20201119"
-    acl = "private"
-  
-    versioning {
-      enabled = true
-    }
+  bucket = "my-example-20201119"
+  acl    = "private"
 
-    tags = {
-      Terraform: "true"
-    }
+  versioning {
+    enabled = true
+  }
+
+  tags = {
+    Terraform : "true"
+    CostCenter : "Dublin"
+  }
 }
